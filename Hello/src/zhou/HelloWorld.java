f8b82aa6eefd9c8eab19fd85f6abf0da8d68c430
324323609 <324323609@C-718T0R1.tdsb.on.ca>
2017-02-21 14:44:20 -0500

    first projec helloworld

---
+++ Hello/src/zhou/HelloWorld.java
@@ -0,0 +1,15 @@
+package zhou;
+/**
+ * HelloWorld.java
+ * prints hello world to console
+ * @author Ethan Zhou
+ *2017/02/21
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
