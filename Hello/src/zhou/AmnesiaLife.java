e10c1785e9ef8cb95a63e6f00c0c755de28becd3
324323609 <324323609@C-718T0R1.tdsb.on.ca>
2017-03-20 16:23:30 -0400

    We think final product.

--- Hello/src/zhou/AmnesiaLife.java
+++ Hello/src/zhou/AmnesiaLife.java
@@ -1,4 +1,12 @@
 package zhou;
+import java.io.IOException;
+import java.io.File;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 /**
  * AmnesiaLife.java
  * Generate adventure game
@@ -22,7 +30,8 @@ public class AmnesiaLife
 		 * 
 		 */
 		//generates console
-		Console c = new Console();
+		String title = "AmnesiaLife";
+		Console c = new Console(40,80,title);
 		c.println("Hello,\n I am your bionic conscious. You are a test subject for a new experimental technology.\n We will send you off, there is a chance that you may lose your memeory. If that does happen, we are not responsible. We will now begin.");
 		int counter =0;
 		//array determines which path player could go after finishing a tree
@@ -42,69 +51,93 @@ public class AmnesiaLife
 			//rock setting starts
 			if(choices[0].equals("rock") && setting[0] == false)
 			{
+				//creates the sound effects
+				try{
+					Clip theme = AudioSystem.getClip(); //create the clip object
+					theme.open(AudioSystem.getAudioInputStream(new File("Teleport Sound Effect.mp3"))); //open the given file for the clip
+
+					Thread.sleep(1000);
+					Thread.sleep(5000);
+					Thread.sleep(500);
+					Thread.sleep(1000);
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+				//imports and displays image
+				BufferedImage jpgImage = null;
 				
+				try {
+					jpgImage = ImageIO.read(new File("resources/cav.jpg"));
+					
+				} catch (IOException e) {
+					System.err.println("There was ana error loading the image.");
+					e.printStackTrace();
+				}
+				c.drawImage (jpgImage, 0, 0, 800, 800, null);
 				c.println("You have awaken in a dark cave, what are you going to do ? (explore/leave)");
 				choices[1] = c.readLine();
 				c.clear();
 				
 				if(choices[1].equals("explore"))
-				{
+				{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 					c.println("You search the walls of the cave and find nothing, do you keep searching ? (yes/no)");
 
 					choices[3] = c.readLine();
 					c.clear();
 					if(choices[3].equals("yes"))
-					{
+					{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 						c.println("You found a portal, do you go though it ? (yes/no)");
 						choices[4] = c.readLine();
 						c.clear();
 						if(choices[4].equals("yes"))
-						{
+						{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 							//creates random number
 							double tele = Math.random()*10;
 
 							if(tele == 4)
-							{
+							{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 								//you win the game, but not sure if the counter would work and how or when it would loop back
 								counter = 3;
 								c.println("You have won the game and now is the ruler of the world. Please play again.");
 							}
 							else
-							{
+							{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 								counter++;
 								c.println("You have gained one piece of your memory, you will be teleported back to where you have began\n Please don't pick rock again, unless you want to go down that road again and find another way \nout.");
 							}
 						}
 						else
-						{
+						{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 							c.println("You have died of starvation. Good luck next time!");
-
+							c.clear();
 
 						}
 					}
 					else
-					{
+					{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 						c.println("You have died of starvation. Good luck next time!");
-
+						c.clear();
 
 					}
 				}
 				else//leave cave
-				{
+				{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 					c.println("You leave the cave, you find a bone, do you pick it up ? (yes/no)");
 					choices[5] = c.readLine();
 					c.clear();
 					if(choices[5].equals("yes"))
-					{
+					{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 						c.println("You encounter a dinosaur and kill it. But then you collapse from exhaustion."
 								+ "You end up waking up back in the room");
 						//counter++;
 						c.println("please dont chose this path again unless you want to find another way to beat it");
 					}
 					else
-					{
+					{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 						c.println("you incounter a dinosaur and can't defend your self. You end up dying. Sucks to be you.");
-
+						c.clear();
 
 					}
 				}
@@ -112,22 +145,44 @@ public class AmnesiaLife
 			//easy button starts (future)
 			else if (choices[0].equals ("easy button") && setting [1] == false)
 			{
+				try{
+					Clip theme = AudioSystem.getClip(); //create the clip object
+					theme.open(AudioSystem.getAudioInputStream(new File("Teleport Sound Effect.mp3"))); //open the given file for the clip
+
+					Thread.sleep(1000);
+					Thread.sleep(5000);
+					Thread.sleep(500);
+					Thread.sleep(1000);
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+				BufferedImage jpgImage = null;
 				
+				try {
+					jpgImage = ImageIO.read(new File("resources/spc.jpg"));
+					
+				} catch (IOException e) {
+					System.err.println("There was ana error loading the image.");
+					e.printStackTrace();
+				}
+				c.drawImage (jpgImage, 0, 0, 800, 800, null);
 				c.println("You wake up in a  pod. You move around and realize you are stuck inside,\n it is also becoming harder to breath. Where do you look ? (head/feet)");
 				choices[6] = c.readLine();
 				c.clear();
 				if(choices[6].equals("head"))
-				{
+				{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 					c.println("You have just found a bar and broken yourself free.\n You look around and notice another pod has been opened. Do you look around and look for the other person? (yes/no");
 					choices[7] = c.readLine();
 					c.clear();
 					if(choices[7].equals("yes"))
-					{
+					{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 						c.println("You found a girl collpased on the floor, do you help her ? (yes/no");
 						choices[8] = c.readLine();
 						c.clear();
 						if (choices[8].equals("yes"))
-						{
+						{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 							c.println("she wakes and warns you danger ahead, she leads you back to the room.");
 							//make this section of code twice
 							do {
@@ -136,36 +191,38 @@ public class AmnesiaLife
 								choices[9] = c.readLine();
 								c.clear();
 								if (choices[9].equals("Microwave")||choices[9].equals("microwave"))
-								{
+								{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 									c.println("you find glasses, you put them on, and wake up back in the first room, you now know what"
 											+ " year it is.");
 									counter++;
 									c.println("please dont chose this path again unless you want to find another way to beat it");
 								}
 								else
-								{
+								{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 									c.println("you find nothing, search again.");
 								}
 							}
 							while ( choices[9].equals ("desk"));
 						}
 						else
-						{
+						{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 							c.println("you continue on, but encounter a fire ahead. you die. ");
-							//c.clear();
+							
+							c.clear();
+							
 
 						}
 					}
 					
 					else
-					{
+					{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 						do {
 							c.println("you see a microwave and a desk, which do you search (Microwave/Desk)");
 
 							choices[9] = c.readLine();
 							c.clear();
 							if (choices[9].equals("Microwave")||choices[9].equals("microwave"))
-							{
+							{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 								c.println("you find glasses, you put them on, and wake up back in the first room, you now know what"
 										+ " year it is.");
 								counter++;
@@ -173,7 +230,7 @@ public class AmnesiaLife
 
 							}
 							else
-							{
+							{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 								c.println("you find nothing, search again.");
 							}
 						}
@@ -181,19 +238,45 @@ public class AmnesiaLife
 					}
 				}
 				else 
-				{
+				{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 					c.println("you find nothing and die from suffocation. better luck next time");
+					c.clear();
 				}
 			}
 			//apocalypse starts (setting)
 			else if (setting[2] == false)
 			{
+				try{
+				Clip theme = AudioSystem.getClip(); //create the clip object
+				theme.open(AudioSystem.getAudioInputStream(new File("Teleport Sound Effect.mp3"))); //open the given file for the clip
+
+				Thread.sleep(1000);
+				Thread.sleep(5000);
+				Thread.sleep(500);
+				Thread.sleep(1000);
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+
+
+				BufferedImage jpgImage = null;
 				
+				try {
+					jpgImage = ImageIO.read(new File("resources/app.jpg"));
+					
+				} catch (IOException e) {
+					System.err.println("There was ana error loading the image.");
+					e.printStackTrace();
+				}
+				c.drawImage (jpgImage, 0, 0, 800, 800, null);
+			
 				c.println("you wake up on a barren street, you see a hospital and the street ahead,/n which way do you go?(street/hospital");
 				choices[10] = c.readLine();
 				c.clear();
 				if (choices[10].equals("street"))
-				{
+				{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 
 
 
@@ -202,13 +285,14 @@ public class AmnesiaLife
 
 
 					c.println("You got over run by zombie and die. try again.");
+					c.clear();
 				}
 
 
 
 
 				else
-				{
+				{c.drawImage (jpgImage, 0, 0, 800, 800, null);
 					c.println("You find a book, while reading it you become dizzy and awake back in the room. You get another piece of memory. ");
 					counter++;
 					c.println("please dont chose this path again unless you want to find another way to beat it");
@@ -217,24 +301,30 @@ public class AmnesiaLife
 			if(choices[0].equals("rock"))
 			{
 				setting[0] = true;
+				c.println("you cannot pick rock again.");
 			}
 			else if (choices[0].equals("easy button"))
 			{
 				setting[1] = true;
+				c.println("you cannot pick easy button again.");
 			}
 			else
 			{
 				setting[2] = true;
+				c.println("you cannot pick cigarette again.");
 			}
 		
 		}
 		
 		while (counter <= 3);
+		c.clear();
+		c.println("You May Have Just Won IDK");
+		
 		//{
 		//	c.println("YOU WIN GOOD JOB!");
 		//}
 
 
-	}
+	}//main method
 
 }
